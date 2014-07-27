6b379cacdd60ecece8f4d0cd367fb670d75d04f7
Karthik Ramasamy <kramasamy@twitter.com>
2014-07-27 08:58:54 -0700

    added line by line comments in the code for better readability

--- lesson1a/src/jvm/udacity/storm/WordCountTopology.java
+++ lesson1a/src/jvm/udacity/storm/WordCountTopology.java
@@ -37,37 +37,62 @@ public class WordCountTopology {
    * A spout that emits a random word
    */
   static class WordSpout extends BaseRichSpout {
-    private Random rnd;
+
+    // Random number generator
+    private Random rnd;  
+
+    // To output tuples from spout to the next stage
     private SpoutOutputCollector collector;
-    @Override
-    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
-      outputFieldsDeclarer.declare(new Fields("word"));
-    }
+
+    // For storing the list of words to be fed into the topology
+    private String[] wordList;
 
     @Override
     public void open(Map map, TopologyContext topologyContext,
                      SpoutOutputCollector spoutOutputCollector) {
+
+      // initialize the random number generator
       rnd = new Random(31);
+
+      // save the output collector provided
       collector = spoutOutputCollector;
+
+      // initialize a set of words
+      wordList = new String[]{"Jack", "Mary", "Jill", "McDonald"};
     }
 
     @Override
     public void nextTuple() {
-      String[] list = {"Jack", "Mary", "Jill", "McDonald"};
+
+      // sleep a second before emitting any word
       Utils.sleep(1000);
-      int nextInt = rnd.nextInt(list.length);
-      collector.emit(new Values(list[nextInt]));
+
+      // generate a random number based on the wordList length 
+      int nextInt = rnd.nextInt(wordList.length);
+
+      // emit the word chosen by the random number from wordList
+      collector.emit(new Values(wordList[nextInt]));
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+      outputFieldsDeclarer.declare(new Fields("word"));
     }
   }
 
   /**
    * A bolt that counts the words that it receives
    */
-  static class ConsumerBolt extends BaseRichBolt {
+  static class CountBolt extends BaseRichBolt {
+
     private OutputCollector collector;
+
+    // Map to store the count of the words
     private Map<String, Integer> countMap;
+
+    @Override
     public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
-      collector = outputCollector;
+      // collector = outputCollector;
       countMap = new HashMap<String, Integer>();
     }
 
@@ -80,7 +105,7 @@ public class WordCountTopology {
         Integer val = countMap.get(key);
         countMap.put(key, ++val);
       }
-      collector.ack(tuple);
+      // collector.ack(tuple);
     }
 
     @Override
@@ -90,30 +115,48 @@ public class WordCountTopology {
 
   public static void main(String[] args) throws Exception {
 
+    // create the topology
     TopologyBuilder builder = new TopologyBuilder();
 
-    builder.setSpout("spout", new WordSpout(), 5);
+    // attach the word spout to the topology - parallelism of 5
+    builder.setSpout("word-spout", new WordSpout(), 5);
 
-    // builder.setBolt("split", new SplitSentence(), 8).shuffleGrouping("spout");
-    builder.setBolt("count", new ConsumerBolt(), 15).fieldsGrouping("spout", new Fields("word"));
+    // attach the count bolt using fields grouping - parallelism of 15
+    builder.setBolt("count-bolt", new CountBolt(), 15).fieldsGrouping("spout", new Fields("word"));
 
+    // create the default config object
     Config conf = new Config();
+
+    // set the config in debugging mode
     conf.setDebug(true);
 
-
     if (args != null && args.length > 0) {
+
+      // run it in a live cluster
+
+      // set the number of workers for running all spout and bolt tasks
       conf.setNumWorkers(3);
 
+      // create the topology and submit with config
       StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
-    }
-    else {
+
+    } else {
+
+      // run it in a simulated local cluster
+
+      // set the number of threads to run - similar to setting number of workers in live cluster
       conf.setMaxTaskParallelism(3);
 
+      // create the local cluster instance
       LocalCluster cluster = new LocalCluster();
+
+      // submit the topology to the local cluster
       cluster.submitTopology("word-count", conf, builder.createTopology());
 
+      // let the topology run for 10 seconds. note topologies never terminate!
       Thread.sleep(10000);
 
+      // we are done, so shutdown the local cluster
       cluster.shutdown();
     }
   }
